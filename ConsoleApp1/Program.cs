500009f5e095364651de6222d62a66a1317bb445
RuszoCsoki <ruszo.csoki@gmail.com>
2023-06-06 23:36:10 -0400

    This is a code first console application, creating a small school database.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp1;
+using EF6Console;
+using System.Data.Entity;
+
+
+using (var ctx = new SchoolContext())
+{
+    var grds = new Grade() { GradeName = "A", GradeId = 1, Section = "85+" };
+    var stud = new Student() { StudentID = 101 ,StudentName = "Bill Bow" , Height = 6 };
+
+
+    ctx.Students.Add(stud);
+    ctx.SaveChanges();
+    Console.WriteLine(stud.StudentName+ " " + stud.StudentID);
+    Console.WriteLine();
+}
+
+namespace EF6Console
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
